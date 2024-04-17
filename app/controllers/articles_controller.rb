76e780390bf24102b046de7dff6c6d5a2b9d691a
Patrik Ferreira <patrikferreira@seepmode.com>
2024-04-17 16:47:27 +0100

    [Adjustments]: clean code refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
+    def index
+        @articles = Article.all
+    end
+
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -12,7 +17,6 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-      @article = Article.find(params[:id])
     end
 
 
@@ -26,7 +30,6 @@ class ArticlesController < ApplicationController
         end
     end
     def update
-      @article = Article.find(params[:id])
       if @article.update(article_params)
         flash[:success] = "Article was successfully created"
           redirect_to article_path(@article)
@@ -36,13 +39,16 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-      @article = Article.find(params[:id])
       @article.destroy
       redirect_to articles_path
     end
 
 
     private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
         params.require(:article).permit(:title, :description)
     end
