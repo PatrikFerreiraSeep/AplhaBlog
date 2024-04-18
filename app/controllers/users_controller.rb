7f6af4a23a9c85f1f3c54bbf429a7311d4f8cf5f
Patrik Ferreira <patrikferreira@seepmode.com>
2024-04-18 15:07:32 +0100

    [Feature]: added edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User was successfully created"
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
   def create
    @user = User.new(user_params)
    if @user.save
